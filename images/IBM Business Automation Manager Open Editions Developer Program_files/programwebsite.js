3aed95493ab06937aa20fa7b1593959914ebcede
Barry Lulas <blulas@gen-065-035-072-15.res.spectrum.com>
2023-11-02 08:09:20 -0400

    removed unused images
    Signed-off-by: Barry Lulas <blulas@gen-065-035-072-15.res.spectrum.com>

--- images/IBM Business Automation Manager Open Editions Developer Program_files/programwebsite.js
+++
@@ -1,7 +0,0 @@
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<script type="text/javascript" language="javascript">var loc=window.location.href;document.cookie="WASReqURLOidcp2026023496="+loc+"; path=/;"</script><script type="text/javascript" language="javascript">window.location.replace("https://login.ibm.com/oidc/endpoint/default/authorize?scope=openid&response_type=code&client_id=NzQ0MjFiODAtMzEzNi00&redirect_uri=https%3A%2F%2Fepwt-www.mybluemix.net%2Foidcclient%2Fredirect%2Fepwt_www_prod_ci&state=001697667635696pTji2ZElI")</script>
-<title>Redirect To OP</title> 
-</head>
-<body></body>
-</html>
